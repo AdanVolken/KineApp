b3a1f6f5f8066744e1356e5dc653d2170de57090
AdanVolken <rolfivolken9@gmail.com>
2024-06-20 17:39:58 -0300

    Finis Desing SintomaIdPage

--- View/SintomaIdPage.xaml.cs
+++ View/SintomaIdPage.xaml.cs
@@ -28,10 +28,17 @@ namespace KineApp.Views
             // Se sacra las partes a las que pertenece el Sintoma
             var sintomaParte = _databaseService.GetItems<SintomaParte>().Where(sp => sp.IdSintoma == _sintomaId).Select(sp => sp.IdParte).ToList();
             var parte = _databaseService.GetItems<ParteModel>().Where(p => sintomaParte.Contains(p.IdParte)).ToList();
+            MusculosCollectionView.ItemsSource = parte;
 
             // Sacamos los Muscuos que pueden tener ese sintoma
             var sintomaMusculo = _databaseService.GetItems<MusculoSintomaModel>().Where(sm => sm.IdSintoma == _sintomaId).Select(sm => sm.IdMusculo).ToList();  
             var musculo =_databaseService.GetItems<MusculoModel>().Where(m => sintomaMusculo.Contains(m.IdMusculo)).ToList();
+            SintomasCollectionView.ItemsSource = musculo;
+
+        }
+
+        private void Button_Clicked_Parte(object sender, EventArgs e)
+        {
 
         }
     }
