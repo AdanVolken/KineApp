21de236a831dd4adc1f560c49c59956a51fedf83
AdanVolken <rolfivolken9@gmail.com>
2024-06-21 11:01:33 -0300

    Create ClienteModel

---
+++ Model/ClienteModel.cs
@@ -0,0 +1,24 @@
+﻿using SQLite;
+using System.ComponentModel;
+
+namespace KineApp.Model
+{
+    [SQLite.Table("Cliente")]
+    class ClienteModel
+    {
+        [PrimaryKey, AutoIncrement]
+        public int IdCliente   { get; set; }    
+
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public string Dni {  get; set; }
+        public string Telefono { get; set; }
+        public string Correo { get; set; }
+        public string ObraSocial { get; set; }
+        public int Sesiones {  get; set; }
+        public string Dolor { get; set; }
+        public int IdMusculo { get; set; }
+        public  int Estampillado { get; set; }
+
+    }
+}
